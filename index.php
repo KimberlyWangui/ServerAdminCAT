436394842f16001336bc272f454881a480ebbf30
Kimberly Wangui <kimberly.wangui@strathmore.edu>
2024-10-02 23:37:09 +0300

    Modified Index page

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <script src="https://kit.fontawesome.com/804a3d4ad2.js"crossorigin="anonymous"></script>
 </head>
 <body>
-    login Successful
+<div class="topnav">
+        <a href="index.php">Home</a>
+    
+        <div class="topnav-right">
+        <a href="Signup.php">Sign Up</a>
+        <a href="Login.php">Login</a>
+    </div>
+    <div class="content_row row">
+        <P>Login Successful</P>
+    </div>
 </body>
 </html>
